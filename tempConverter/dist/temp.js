19a85f54f1ee962e67a449158ebc74475cbfd524
AnilHasda <hasdaanil098@gmail.com>
2024-06-03 17:24:01 +0545

    temperature converter added

--- tempConverter/dist/temp.js
+++ tempConverter/dist/temp.js
@@ -10,4 +10,18 @@ class Temperature {
     set celcius(degree) {
         this._celcius = degree;
     }
+    //   this getter method will convert celcius to fahreinheit
+    get fahreinheit() {
+        console.log("i am clicked");
+        return (this.celcius * 9) / 5 + 32;
+    }
+    //   this setter method will convert fahreinheit to celcius
+    set fahreinheit(fahrein) {
+        this._celcius = ((fahrein - 32) * 5) / 9;
+    }
 }
+// creating object for temperature class
+let data = new Temperature(20);
+console.log(data.fahreinheit);
+data.fahreinheit = 68;
+console.log(data.celcius);
--- tempConverter/src/temp.ts
+++ tempConverter/src/temp.ts
@@ -1,9 +1,23 @@
-class Temperature{
-    constructor(private _celcius:number){}
-    public get celcius(){
-        return this._celcius;
-    }
-    public set celcius(degree){
-        this._celcius=degree;
-    }
+class Temperature {
+  constructor(private _celcius: number) {}
+  public get celcius() {
+    return this._celcius;
+  }
+  public set celcius(degree) {
+    this._celcius = degree;
+  }
+  //   this getter method will convert celcius to fahreinheit
+  public get fahreinheit(): number {
+    console.log("i am clicked");
+    return (this.celcius * 9) / 5 + 32;
+  }
+  //   this setter method will convert fahreinheit to celcius
+  public set fahreinheit(fahrein: number) {
+    this._celcius = ((fahrein - 32) * 5) / 9;
+  }
 }
+// creating object for temperature class
+let data = new Temperature(20);
+console.log(data.fahreinheit);
+data.fahreinheit = 68;
+console.log(data.celcius);
