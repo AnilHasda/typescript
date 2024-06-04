c5aea7bc39bbc50198e33a8462a3261c4965ada0
AnilHasda <hasdaanil098@gmail.com>
2024-06-04 14:23:46 +0545

    small search user project

--- searchUser/dist/app.js
+++ searchUser/dist/app.js
@@ -1,2 +1,40 @@
 "use strict";
-console.log("hello anil hasda ");
+let input = document.querySelector(".input");
+let main_container = document.querySelector(".main-container");
+console.log("hello this is anil hasda");
+// fetcher function
+const fetcher = async (url) => {
+    let response = await fetch(url);
+    let result = await response.json();
+    return result;
+};
+let responseData = fetcher("https://api.github.com/users").then((response) => {
+    response.forEach((ele) => {
+        //  let  element:HTMLElement= document.createElement("div");
+        //  element.setAttribute("class","element");
+        main_container.innerHTML += `
+  <div class="element">
+  <img src=${ele.avatar_url} alt=${ele.login} width="100%"></img>
+  <div class="user_name">Name:${ele.login}<div>
+  <a href=${ele.url} target="blank">View Detail</a>
+  </div>
+  `;
+    });
+    // console.log(response);
+});
+const searchUser = async (e) => {
+    main_container.innerHTML = "";
+    e.preventDefault();
+    let response = await fetcher("https://api.github.com/users");
+    let filterData = response.filter(ele => ele.login.includes(input.value));
+    (filterData.length > 0) ?
+        filterData.forEach(ele => {
+            main_container.innerHTML += `
+    <div class="element">
+    <img src=${ele.avatar_url} alt=${ele.login} width="100%"></img>
+    <div class="user_name">Name:${ele.login}<div>
+    <a href=${ele.url} target="blank">View Detail</a>
+    </div>
+    `;
+        }) : main_container.innerHTML = `<div>No such data found ! Try another</div>`;
+};
--- searchUser/src/app.ts
+++ searchUser/src/app.ts
@@ -1,1 +1,49 @@
-console.log("hello anil hasda ");
+let input = document.querySelector(".input") as HTMLInputElement;
+let main_container = document.querySelector(".main-container") as HTMLElement;
+console.log("hello this is anil hasda");
+// define structure of an object
+interface responseFormat {
+  id: number;
+  login: string;
+  avatar_url: string;
+  url: string;
+}
+// fetcher function
+const fetcher = async <T>(url: string): Promise<T> => {
+  let response = await fetch(url);
+  let result = await response.json();
+  return result;
+};
+let responseData = fetcher<responseFormat[]>(
+  "https://api.github.com/users"
+).then((response) => {
+  response.forEach((ele) => {
+    //  let  element:HTMLElement= document.createElement("div");
+    //  element.setAttribute("class","element");
+    main_container.innerHTML += `
+  <div class="element">
+  <img src=${ele.avatar_url} alt=${ele.login} width="100%"></img>
+  <div class="user_name">Name:${ele.login}<div>
+  <a href=${ele.url} target="blank">View Detail</a>
+  </div>
+  `;
+  });
+  // console.log(response);
+});
+
+const searchUser = async (e: Event): Promise<void> => {
+  main_container.innerHTML="";
+  e.preventDefault();
+  let response=await fetcher<responseFormat[]>("https://api.github.com/users");
+  let filterData=response.filter(ele=>ele.login.includes(input.value));
+  (filterData.length>0)?
+  filterData.forEach(ele=>{
+    main_container.innerHTML += `
+    <div class="element">
+    <img src=${ele.avatar_url} alt=${ele.login} width="100%"></img>
+    <div class="user_name">Name:${ele.login}<div>
+    <a href=${ele.url} target="blank">View Detail</a>
+    </div>
+    `;
+  }):main_container.innerHTML=`<div>No such data found ! Try another</div>`
+};
